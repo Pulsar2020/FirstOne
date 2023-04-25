fbad039e5ce195e2300d4819932395335a06f393
Kunzhan li <695550612@qq.com>
2023-04-26 06:25:30 +0800

    modify code

--- src/great_class29/Problem_0073_SetMatrixZeroes.java
+++ src/great_class29/Problem_0073_SetMatrixZeroes.java
@@ -6,7 +6,56 @@ package great_class29;
  * @author likz
  */
 public class Problem_0073_SetMatrixZeroes {
-    public static void setZeroes1(int[][] matrix) {
+    public static void setZeroes(int[][] matrix) {
+        boolean rowZeroFlag = false;
+        boolean colZeroFlag = false;
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                rowZeroFlag = true;
+                break;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                colZeroFlag = true;
+                break;
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
 
+        for (int i = 1; i < matrix.length ; i++){
+            if (matrix[i][0] == 0){
+                for (int j = 1; j < matrix[0].length; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int j = 1; j < matrix[0].length; j++){
+            if (matrix[0][j] == 0){
+                for (int i = 1; i < matrix.length; i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (rowZeroFlag) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (colZeroFlag) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
