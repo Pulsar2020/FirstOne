68a5c6e33ccea38869290052396718970b98d3b9
likz <likz@dragoninfo.com.cn>
2023-05-12 09:39:08 +0800

    feat: my algorithm practice

--- src/great_class30/Problem_0116_PopulatingNextRightPointersInEachNode.java
+++ src/great_class30/Problem_0116_PopulatingNextRightPointersInEachNode.java
@@ -6,4 +6,73 @@ package great_class30;
  * @author likz
  */
 public class Problem_0116_PopulatingNextRightPointersInEachNode {
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+    }
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        MyQueue queue = new MyQueue();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size;
+            Node pre = null;
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+                if (pre != null){
+                    pre.next = cur;
+                }
+                pre = cur;
+            }
+        }
+        return root;
+    }
+
+
+    public static class MyQueue {
+        public Node head;
+        public Node tail;
+        public int size;
+
+        public MyQueue() {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public void offer(Node node) {
+            size++;
+            if (head == null) {
+                head = node;
+                tail = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+        }
+
+        public Node poll() {
+            size--;
+            Node cur = head;
+            head = head.next;
+            cur.next = null;
+            return cur;
+        }
+
+    }
 }
