9d327b66aec87643ab4584015f348537379db8a5
Kunzhan li <695550612@qq.com>
2023-05-15 22:21:12 +0800

    modify code

--- src/code/LargestRectangleInHistogram.java
+++ src/code/LargestRectangleInHistogram.java
@@ -27,4 +27,26 @@ public class LargestRectangleInHistogram {
         return maxArea;
     }
 
+    public static int largestRectangleArea2(int[] heights) {
+        if(heights == null || heights.length == 0){
+            return 0;
+        }
+        int maxArea = Integer.MIN_VALUE;
+        int[] stack = new int[heights.length];
+        int size = -1;
+        for (int i = 0; i < heights.length; i++){
+            while(size >= 0 && heights[i] <= heights[stack[size]]){
+                int j = stack[size--];
+                int k = size == -1 ? -1 : stack[size];
+                maxArea = Math.max(maxArea, (i - k - 1) * heights[j]);
+            }
+            stack[++size] = i;
+        }
+        while(size >= 0 ){
+            int j = stack[size--];
+            int k = size == -1 ? -1 : stack[size];
+            maxArea = Math.max(maxArea, (heights.length - k - 1) * heights[j]);
+        }
+        return maxArea;
+    }
 }
